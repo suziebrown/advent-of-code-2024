73a5eb4224f2e731414ef98889ac1510074705d3
Suzie Brown <suzie.brown42@gmail.com>
2024-12-01 10:23:23 +0000

    day 1 part 2

--- day01.test.ts
+++ day01.test.ts
@@ -1,12 +1,18 @@
 import { describe, it } from "node:test";
 import { readFile } from './read-file';
 import * as assert from "node:assert/strict";
-import { day1part1 } from "./day01";
+import { day1part1, day1part2 } from "./day01";
 
 describe('Day 1', () => {
-    it('solves with test input', () => {
+    it('Part 1', () => {
         const result = day1part1("day01-test");
 
         assert.equal(result, 11);        
+    });
+
+    it('Part 2', () => {
+        const result = day1part2("day01-test");
+
+        assert.equal(result, 31);        
     });
 });
--- day01.ts
+++ day01.ts
@@ -4,7 +4,7 @@ const readData = (filename: string): { leftNumbers: number[], rightNumbers: numb
     const rawData = readFile(filename);
     const lines = rawData.split('\n');
 
-    const leftNumbers = lines.slice().map(l => Number(l.split('  ')[0].trim()));
+    const leftNumbers = lines.map(l => Number(l.split('  ')[0].trim()));
     const rightNumbers = lines.map(l => Number(l.split('  ')[1].trim()));
 
     return { leftNumbers, rightNumbers };
@@ -16,11 +16,17 @@ const calculateDifferences = (leftNumbers: number[], rightNumbers: number[]): nu
 
     let differences = [];
 
-    for (var i = 0; i < leftNumbers.length; i++){
+    for (var i = 0; i < leftNumbers.length; i++) {
         differences[i] = Math.abs(rightNumbers[i] - leftNumbers[i]);
     }
 
     return differences;
+}
+
+const calculateSimilarities = (leftNumbers: number[], rightNumbers: number[]): number[] => {
+    const similarities = leftNumbers.map(l => l * rightNumbers.filter(r => r === l).length);
+    
+    return similarities;
 }
 
 export const day1part1 = (filename: string): number => {
@@ -28,8 +34,16 @@ export const day1part1 = (filename: string): number => {
     const differences = calculateDifferences(data.leftNumbers, data.rightNumbers);
 
     const sum = differences.reduce((a,b) => a + b);
-    console.log({ sum });
     return sum;
 }
 
-day1part1("day01");
+export const day1part2 = (filename: string): number => {
+    const data = readData(filename);
+    const similarities = calculateSimilarities(data.leftNumbers, data.rightNumbers);
+
+    const sum = similarities.reduce((a,b) => a + b);
+    return sum;
+}
+
+console.log("Part 1: " + day1part1("day01"));
+console.log("Part 2: " + day1part2("day01"));
