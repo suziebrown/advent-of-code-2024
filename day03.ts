d5e220bdc9dded99548f64261b8004f9e01c15e1
Suzie Brown <suzie.brown42@gmail.com>
2024-12-03 09:15:01 +0000

    day 3 part 2

--- day03.ts
+++ day03.ts
@@ -6,14 +6,14 @@ const readData = (filename: string): string => {
     return rawData;
 }
 
-const instructionRegex = /mul\(\d{1,3},\d{1,3}\)/g;
+const multiplicationInstructionRegex = /mul\(\d{1,3},\d{1,3}\)/g;
 
-const findInstructions = (rawData: string): string[] => {
-    const matches = rawData.matchAll(instructionRegex);
+const findMultiplicationInstructions = (rawData: string): string[] => {
+    const matches = rawData.matchAll(multiplicationInstructionRegex);
     return [...matches].map(m => String(m));
 }
 
-const executeInstruction = (instruction: string): number => {
+const executeMultiplicationInstruction = (instruction: string): number => {
     const numbers = instruction.replace("mul(", ""). replace(")", "").split(",");
     const leftNumber = Number(numbers[0]);
     const rightNumber = Number(numbers[1]);
@@ -21,20 +21,46 @@ const executeInstruction = (instruction: string): number => {
     return leftNumber * rightNumber;
 }
 
+const multiplicationOrConditionalRegex = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g;
+
+const findMultiplicationAndConditionalInstructions = (rawData: string): string[] => {
+    const matches = rawData.matchAll(multiplicationOrConditionalRegex);
+    return [...matches].map(m => String(m));
+}
+
+const conditionallyExecuteMultiplications = (allInstructions: string[]): number[] => {
+    const results: number[] = [];
+    let multiplicationEnabled = true;
+
+    for (var i = 0; i < allInstructions.length; i++) {
+        const instruction = allInstructions[i];
+        if (instruction === "do()") {
+            multiplicationEnabled = true;
+        } else if (instruction === "don't()") {
+            multiplicationEnabled = false;
+        } else if (multiplicationEnabled) {
+            results.push(executeMultiplicationInstruction(instruction));
+        }
+    }
+
+    return results;
+}
 
 export const day3part1 = (filename: string): number => {
     const rawData = readData(filename);
-    const instructions = findInstructions(rawData);
-    const results = instructions.map(i => executeInstruction(i));
+    const instructions = findMultiplicationInstructions(rawData);
+    const results = instructions.map(i => executeMultiplicationInstruction(i));
     
     return results.reduce((a, b) => a + b);
 }
 
 export const day3part2 = (filename: string): number => {
-    const reports = readData(filename);
-    
-    return 0;
+    const rawData = readData(filename);
+    const allInstructions = findMultiplicationAndConditionalInstructions(rawData);
+    const results = conditionallyExecuteMultiplications(allInstructions);
+
+    return results.reduce((a, b) => a + b);
 }
 
 console.log("Day 3 Part 1: " + day3part1("day03"));
-// console.log("Day 3 Part 2: " + day3part2("day03"));
+console.log("Day 3 Part 2: " + day3part2("day03"));
